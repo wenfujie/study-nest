dddf456ec5afc11e8a09355b4f61ed3be27a7ec7
温富杰 <wenfujie@dianchu.com>
2023-11-21 17:56:31 +0800

    feat: 重新初始化数据库shema.prisma文件

--- prisma/migrations/20231121095520_init/migration.sql
+++ prisma/migrations/20231121095520_init/migration.sql
@@ -1,22 +1,3 @@
-/*
-  Warnings:
-
-  - The primary key for the `Post` table will be changed. If it partially fails, the table could be left without primary key constraint.
-  - You are about to drop the column `author` on the `Post` table. All the data in the column will be lost.
-  - You are about to alter the column `id` on the `Post` table. The data in that column could be lost. The data in that column will be cast from `VarChar(191)` to `Int`.
-  - Added the required column `courseId` to the `Post` table without a default value. This is not possible if the table is not empty.
-
-*/
--- DropIndex
-DROP INDEX `Post_title_key` ON `Post`;
-
--- AlterTable
-ALTER TABLE `Post` DROP PRIMARY KEY,
-    DROP COLUMN `author`,
-    ADD COLUMN `courseId` INTEGER NOT NULL,
-    MODIFY `id` INTEGER NOT NULL AUTO_INCREMENT,
-    ADD PRIMARY KEY (`id`);
-
 -- CreateTable
 CREATE TABLE `User` (
     `id` VARCHAR(191) NOT NULL,
@@ -30,6 +11,20 @@ CREATE TABLE `Course` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `title` VARCHAR(191) NOT NULL,
     `coverImage` VARCHAR(191) NOT NULL,
+    `createTime` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updateTime` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Post` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+    `content` VARCHAR(191) NULL,
+    `createTime` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updateTime` DATETIME(3) NOT NULL,
+    `courseId` INTEGER NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -52,6 +47,15 @@ CREATE TABLE `PostsOnUsers` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
+CREATE TABLE `MenuItem` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `label` VARCHAR(191) NOT NULL,
+    `parentId` INTEGER NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
 CREATE TABLE `_CourseToMenuItem` (
     `A` INTEGER NOT NULL,
     `B` INTEGER NOT NULL,
@@ -70,10 +74,13 @@ ALTER TABLE `CoursesOnUsers` ADD CONSTRAINT `CoursesOnUsers_courseId_fkey` FOREI
 ALTER TABLE `CoursesOnUsers` ADD CONSTRAINT `CoursesOnUsers_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE `PostsOnUsers` ADD CONSTRAINT `PostsOnUsers_postId_fkey` FOREIGN KEY (`postId`) REFERENCES `Post`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE `PostsOnUsers` ADD CONSTRAINT `PostsOnUsers_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `PostsOnUsers` ADD CONSTRAINT `PostsOnUsers_postId_fkey` FOREIGN KEY (`postId`) REFERENCES `Post`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `MenuItem` ADD CONSTRAINT `MenuItem_parentId_fkey` FOREIGN KEY (`parentId`) REFERENCES `MenuItem`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `_CourseToMenuItem` ADD CONSTRAINT `_CourseToMenuItem_A_fkey` FOREIGN KEY (`A`) REFERENCES `Course`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
